57199a25c78627dbaa8ba97998b0954bab8e6704
owalker10 <36974651+owalker10@users.noreply.github.com>
2020-10-03 18:43:10 -0500

    refactored to client dir

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const bodyParser= require('body-parser')
+
+/*
+to deploy:
+
+'npm run build' in /web/client
+'cd ../..'
+'heroku login'
+'heroku git:remote -a billboard-viz'
+'git subtree push --prefix web heroku master'
+
+*/
+
+const app = express();
+
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// An api endpoint that returns a short list of items
+
+
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log('App is listening on port ' + port);
